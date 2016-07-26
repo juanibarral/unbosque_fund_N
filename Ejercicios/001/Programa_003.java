477087a41489fbdddf1adfdd3ce4b9d0bdc5a7b3
juanibarral <juanibarral@gmail.com>
2016-07-26 16:49:05 -0500

    Cambios en formato presentación 4

---
+++ Ejercicios/001/Programa_003.java
@@ -0,0 +1,8 @@
+public class Programa_003{
+	public static void main(String[] args)
+	{
+		System.out.println("Ingrese su nombre");
+		String input = System.console().readLine();
+		System.out.println("Hola " + input);
+	}
+}
