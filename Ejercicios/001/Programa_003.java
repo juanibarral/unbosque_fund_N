e44dc580f59ff12c5fcfc3f04116caa0511aed94
Juan Camilo Ibarra Lopez <juanibarral@gmail.com>
2016-07-29 16:10:06 -0500

    Delete Programa_003.java

--- Ejercicios/001/Programa_003.java
+++
@@ -1,8 +0,0 @@
-public class Programa_003{
-	public static void main(String[] args)
-	{
-		System.out.println("Ingrese su nombre");
-		String input = System.console().readLine();
-		System.out.println("Hola " + input);
-	}
-}
